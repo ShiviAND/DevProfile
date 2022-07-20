60803849ad634fcbfa565871358274f218fc5100
Shivender <shivender@livelike.com>
2022-07-20 08:54:29 +0530

    mvvm room

--- app/src/main/java/com/example/devprofile/model/Data.kt
+++
@@ -1,5 +0,0 @@
-package com.example.devprofile.model
-
-data class Data(
-    val memes: List<Meme>
-)
--- app/src/main/java/com/example/devprofile/model/Jokes.kt
+++
@@ -1,6 +0,0 @@
-package com.example.devprofile.model
-
-data class Jokes(
-    val `data`: Data,
-    val success: Boolean
-)
